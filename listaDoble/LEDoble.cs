349ba654e80840fd5c9e4bc37990a92b6ef15bd8
bojorquezja <bojorquez.ja@gmail.com>
2025-04-10 18:06:49 -0500

    codigo lleno en tostring y agregafinal

--- listaDoble/LEDoble.cs
+++ listaDoble/LEDoble.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace listaDoble {
     internal class LEDoble {
@@ -16,13 +17,28 @@ namespace listaDoble {
 
         //agrega un valor al final de LE Doble
         public void AgregaFinal(int valor) {
+            Nodo nuevo = new Nodo(valor);
             
+            if (this.Ultimo == null) {//no hay nodos
+                this.Primero = nuevo;
+                this.Ultimo = nuevo;
+            } else {
+                Nodo ult = this.Ultimo;
+                this.Ultimo = nuevo;
+                ult.Sig = nuevo;
+                nuevo.Ant = ult;
+            }
         }
 
         //ve valores separados por comas de la LEDoble
         public override string ToString() {
-            
-            return "";
+            string listaVag = "";
+            Nodo tmp = this.Primero;
+            while (tmp != null) {
+                listaVag += tmp.Dato + ", ";
+                tmp = tmp.Sig;
+            }
+            return listaVag;
         }
     }
 }
