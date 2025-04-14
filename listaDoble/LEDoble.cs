bc4cad3ea482d5ce464bc51627b5c55ba3256141
bojorquezja <bojorquez.ja@gmail.com>
2025-04-14 11:21:08 -0500

    nuevos metodos

--- listaDoble/LEDoble.cs
+++ listaDoble/LEDoble.cs
@@ -15,6 +15,21 @@ namespace listaDoble {
 
         }
 
+        //elimina un valor al inicio de LE Doble
+        public void EliminaPrimero(int valor) {
+
+        }
+
+        //retorna el valor de una pocicion de la LEDoble iniciando por la posicion cero
+        public int GetValor(int pos) {
+            return 0;
+        }
+
+        //Retorn la cantidad de valores que tiene la LEDoble
+        public int Largo() {
+            return 0;
+        }
+
         //agrega un valor al final de LE Doble
         public void AgregaFinal(int valor) {
             Nodo nuevo = new Nodo(valor);
